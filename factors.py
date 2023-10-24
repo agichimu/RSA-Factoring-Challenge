b9d8b5789a85835df4469ef179cc84530ae19ece
agichimu <alexandergichimu98@gmail.com>
2023-10-24 17:39:32 +0300

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,33 +1,20 @@
+#!/usr/bin/python3
+"""Factorize as many numbers as possible
+into a product of two smaller numbers.
+"""
 import sys
 
-def factorize_number(n):
-    factors = []
-    divisor = 2
-    while n > 1:
-        while n % divisor == 0:
-            factors.append(divisor)
-            n //= divisor
-        divisor += 1
-    return factors
+with open(sys.argv[1], "r") as fp:
+    Lines = fp.readlines()
+    new_list = [int(i) for i in Lines]
+    new_list.sort()
 
-def main(input_file):
-    try:
-        with open(input_file, 'r') as file:
-            numbers = file.read().splitlines()
-
-        for number in numbers:
-            n = int(number)
-            factors = factorize_number(n)
-            factorization = f"{n}={'*'.join(map(str, factors))}"
-            print(factorization)
-
-    except FileNotFoundError:
-        print(f"Error: Input file '{input_file}' not found.")
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python factors.py <input_file>")
-        sys.exit(1)
-
-    input_file = "alex_numbers.txt" 
-    main(input_file)
+    for line in new_list:
+        d = 2
+        while d < line:
+            if line % d == 0:
+                f = line // d
+                print("{}={}*{}".format(line, f, d))
+                break
+            else:
+                d += 1
